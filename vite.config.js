8ffdeb5d7eac73fb895d6853090613662753053d
YuhanZhou0624 <russellzhou624@gmail.com>
2025-03-20 14:32:18 +1100

    fix issues

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,11 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ['moment-timezone']
+    }
+  },
   plugins: [
     vue(),
     vueDevTools(),
